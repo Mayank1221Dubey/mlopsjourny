fc7de0e7073a5536d907344570b0b1fc45f2fd6e
Mayank1221Dubey <55981174+Mayank1221Dubey@users.noreply.github.com>
2023-09-15 17:47:31 +0530

    Add files via upload

---
+++ datenow.py
@@ -0,0 +1,8 @@
+import datetime
+
+def print_current_date():
+    current_date = datetime.date.today()
+    print("Current date:", current_date)
+
+# Call the function to print the current date
+print_current_date()
---
+++ myinfo.py
@@ -0,0 +1,7 @@
+def display_name():
+    name = "Mayank Dubey"
+    print("My name is:", name)
+
+# Call the function to display your name
+display_name()
+print("I am in Pune right now..")
